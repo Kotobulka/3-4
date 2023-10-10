4cde0be70a0edee716846b386aaa1ca0fbd224c7
Kotobulka <144238123+Kotobulka@users.noreply.github.com>
2023-10-10 09:24:17 +0300

    Add files via upload

---
+++ z12.pas
@@ -0,0 +1,16 @@
+﻿var
+  sum, pro, f, j: int64;
+
+begin
+  sum := 0;
+  j := 1;
+  pro := 1;
+  readln(f);
+  while f <> j - 1 do
+  begin
+    if j mod 2 = 1 then sum := sum + j else pro:=pro*j;
+    j := j + 1;
+  end;
+  writeln(sum);
+  writeln(pro);
+end.
